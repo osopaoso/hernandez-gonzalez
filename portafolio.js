1f2ab4006e063dd06b204ad05eefa14cec14eab3
osopaoso <49135555+osopaoso@users.noreply.github.com>
2019-09-01 21:36:32 -0400

    Update portafolio.js

--- portafolio.js
+++ portafolio.js
@@ -2,7 +2,7 @@ var trabajos = [{
     figure: "media/trabajo-01-Macaws.jpg",
     alt: "Comedero de Guacamayos en Zoo Metropolitano de Chile",
     title: "Comedero para guacamayos",
-    about: "Proyecto que entrega calidad a los guacamayos, sin pasar a llevar sus jerarquias y conductas. <br> -Aldo"
+    about: "Proyecto que entrega calidad a los guacamayos, sin pasar a llevar sus jerarquias y conductas. <br><i> -Aldo</i>"
 }, {
     figure: "media/trabajo-02-Julia.png",
     alt: "Herramienta multiuso Julia desplegada",
@@ -12,7 +12,7 @@ var trabajos = [{
     figure: "media/trabajo-03-Damero.jpg",
     alt: "Corte fugado de proyecto.",
     title: "La reconquista del damero",
-    about: "Plan que busca entregar el interior de una cuadra de santiago centro al fluido público.<br> -Aldo"
+    about: "Plan que busca entregar el interior de una cuadra de santiago centro al fluido público.<br><i> -Aldo</i>"
 }, {
     figure: "media/trabajo-04-editorial.jpg",
     alt: "Descripción de la imagen",
@@ -22,7 +22,7 @@ var trabajos = [{
     figure: "media/trabajo-05-MAT.jpg",
     alt: "Axonometrica ilustrada del producto: Lavaplatos portable",
     title: "MAT",
-    about: "Producto que busca entregar una solucion al lavado de utensilios alimenticios despues de una emergencia.<br> -Aldo"
+    about: "Producto que busca entregar una solucion al lavado de utensilios alimenticios despues de una emergencia.<br><i> -Aldo</i>"
 }, {
     figure: "media/trabajo-06-textil.jpg",
     alt: "Descripción de la imagen",
